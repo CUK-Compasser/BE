c673db4b0cd2b8874e636c45c004c5b6a1a57cab
jijysun <jijysun@naver.com>
2026-01-22 19:31:40 +0900

    PROJECT INIT

---
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/CukCompasserApplication.java
@@ -0,0 +1,13 @@
+package Comprehensive_Design_Project.CUK_Compasser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CukCompasserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CukCompasserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Comprehensive_Design_Project/CUK_Compasser/CukCompasserApplicationTests.java
@@ -0,0 +1,13 @@
+package Comprehensive_Design_Project.CUK_Compasser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CukCompasserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
