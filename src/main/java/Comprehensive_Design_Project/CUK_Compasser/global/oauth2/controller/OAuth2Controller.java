be3bbc41c39ca054ba1a474db5c4bb6c3a931b14
jijysun <jijysun@gmail.com>
2026-02-11 19:44:42 +0900

    feat - 엔티티 셋팅

---
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/global/oauth2/controller/OAuth2Controller.java
@@ -0,0 +1,7 @@
+package Comprehensive_Design_Project.CUK_Compasser.global.oauth2.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OAuth2Controller {
+}
