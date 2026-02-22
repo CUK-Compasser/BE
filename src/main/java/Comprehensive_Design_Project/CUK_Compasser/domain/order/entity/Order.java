6cdd90f874b68a8c9da2fbcae21b88c68ac64fb0
jijysun <jijysun@gmail.com>
2026-02-22 16:11:56 +0900

    fix - 오타 수정 및 엔티티 이름 충돌 수정

--- src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/order/entity/Order.java
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/order/entity/Order.java
@@ -1,11 +1,9 @@
 package Comprehensive_Design_Project.CUK_Compasser.domain.order.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "orders") // order가 예약어
 public class Order {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
