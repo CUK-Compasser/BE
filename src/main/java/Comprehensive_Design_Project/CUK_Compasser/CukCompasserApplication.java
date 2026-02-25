a78551cf2905baf9d7cbd9ad88a011ae2b80eb97
shawn9272 <shawn9272@naver.com>
2026-02-25 19:13:02 +0900

    feat - 운영 및 로컬 모니터링 시스템(Prometheus, Loki, Grafana, Promtail) 구축 및 설정 분리

--- src/main/java/Comprehensive_Design_Project/CUK_Compasser/CukCompasserApplication.java
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/CukCompasserApplication.java
@@ -2,8 +2,16 @@ package Comprehensive_Design_Project.CUK_Compasser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
+
 public class CukCompasserApplication {
 
 	public static void main(String[] args) {
