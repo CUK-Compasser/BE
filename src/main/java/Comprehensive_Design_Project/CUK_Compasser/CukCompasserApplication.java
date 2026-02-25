e65b5bfb16f302843210a7ab2f3ff79803c25667
jijysun <jijysun@naver.com>
2026-02-25 23:27:55 +0900

    feat - jpa 변경 감지를 위한 어노테이션 추가

--- src/main/java/Comprehensive_Design_Project/CUK_Compasser/CukCompasserApplication.java
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/CukCompasserApplication.java
@@ -5,13 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = {
 		DataSourceAutoConfiguration.class,
 		DataSourceTransactionManagerAutoConfiguration.class,
 		HibernateJpaAutoConfiguration.class
 })
-
+@EnableJpaAuditing
 public class CukCompasserApplication {
 
 	public static void main(String[] args) {
