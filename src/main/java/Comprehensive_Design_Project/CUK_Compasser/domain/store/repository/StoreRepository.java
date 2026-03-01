723b03482371e7968ad315ad541faec8209fa5d1
jijysun <jijysun@gmail.com>
2026-03-01 17:07:56 +0900

    feat - 사용자 기준 반경 조회 JPQL 작성

--- src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/store/repository/StoreRepository.java
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/store/repository/StoreRepository.java
@@ -1,8 +1,12 @@
 package Comprehensive_Design_Project.CUK_Compasser.domain.store.repository;
 
 import Comprehensive_Design_Project.CUK_Compasser.domain.store.entity.Store;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
@@ -10,4 +14,11 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
     Optional<Store> findByIdAndStoreManager_MemberId(Long storeId, Long memberId);
 
     Optional<Store> findByStoreManager_MemberId(Long memberId);
+
+
+    @Query("Select s From Store s where st_distance_sphere(Point(:userLat, :userLon), Point(s.latitude, s.longitude)) <= :distance")
+    List<Store> findStoresWithinRadius(
+            @Param("userLat") BigDecimal userLat,
+            @Param("userLon") BigDecimal userLon,
+            @Param("distance") double distanceInMeters);
 }
