978b2b6175acbda4562846299bfb9a9f0ae90356
jijysun <jijysun@naver.com>
2026-03-05 23:10:56 +0900

    feat - response 수정

--- src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/store/controller/StoreController.java
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/store/controller/StoreController.java
@@ -12,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/stores")
@@ -58,14 +60,14 @@ public class StoreController {
 
     @GetMapping // 로그인 이후 바로 연결되는 메인 페이지, createdAt 기준 페이지네이션 10개
     @Operation(summary = "가게 조회 메인 페이지 조회 API", description = "사용자가 로그인 이후 연결되는 메인 가게 조회 API로, createdAt 기준 페이지네이션으로 10개 씩 반환하는 API 입니다.")
-    public ApiResponse<Object> getStoreList (
+    public ApiResponse<List<StoreRespDTO>> getStoreList (
             @AuthenticationPrincipal CustomUserDetails userDetails){
         return null;
     }
 
     @GetMapping("/{tag}") // 태그 별 리스트  조회
     @Operation(summary = "태그 별 가게 조회 API", description = "사용자가 고른 태그를 기준으로 가게를 페이지네이션 조회를 하는 API 입니다.")
-    public ApiResponse<Object> getStoreListByTag (
+    public ApiResponse<List<StoreRespDTO>> getStoreListByTag (
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable String tag
     ){
@@ -74,7 +76,7 @@ public class StoreController {
 
     @GetMapping("/university/{university}") // 대학교 반경 기준 가게 조회 API
     @Operation(summary = "대학교 반경 가게 조회 API", description = "사용자가 고른 대학교를 기준으로 반경의 가게를 조회하는 API 입니다.")
-    public ApiResponse<Object> getStoreListByUniversity (
+    public ApiResponse<List<StoreRespDTO>> getStoreListByUniversity (
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable String university){
         return null;
@@ -82,7 +84,7 @@ public class StoreController {
 
     @GetMapping("/member") // 지도 클릭 시 사용자 반경 가게 조회 API
     @Operation(summary = "사용자 반경 가게 조회 API", description = "사용자의 위치 기준 반경의 가게의 조회하는  API 입니다.")
-    public ApiResponse<Object> getStoreListByMemberRadius (
+    public ApiResponse<List<StoreRespDTO>> getStoreListByMemberRadius (
             @RequestBody MemberReqDTO.MemberCoordinatesDTO coordinates,
             @AuthenticationPrincipal CustomUserDetails userDetails){
         return null;
