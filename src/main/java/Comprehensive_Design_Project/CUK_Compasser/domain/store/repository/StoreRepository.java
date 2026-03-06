b632f2ee020d5f16e285f1a528c09500a89641f2
jijysun <jijysun@naver.com>
2026-03-06 17:01:13 +0900

    feat - 스토어 조회 별 repository 코드 작성 ST_Distance_Sphere는 MySQL 종속 함수로 NativeSQL 사용

--- src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/store/repository/StoreRepository.java
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/store/repository/StoreRepository.java
@@ -1,10 +1,13 @@
 package Comprehensive_Design_Project.CUK_Compasser.domain.store.repository;
 
 import Comprehensive_Design_Project.CUK_Compasser.domain.store.entity.Store;
+import Comprehensive_Design_Project.CUK_Compasser.domain.store.entity.Tag;
 import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.awt.print.Pageable;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
@@ -15,10 +18,46 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 
     Optional<Store> findByStoreManager_MemberId(Long memberId);
 
+    /*
+    * 반경 함수 기준 MySQL = ST_Distance_Sphere = 특정 DB 종속 함수라 NativeSQL이 필요
+    */
 
     @Query("Select s From Store s where st_distance_sphere(Point(:userLat, :userLon), Point(s.latitude, s.longitude)) <= :distance")
     List<Store> findStoresWithinRadius(
             @Param("userLat") BigDecimal userLat,
             @Param("userLon") BigDecimal userLon,
             @Param("distance") double distanceInMeters);
+
+    Page<Store> findAllByOrderByCreatedAtDesc(Pageable pageable); // 기본 조회 + 페이징
+
+    @Query(value = "" +
+            "Select * From store s " +
+            "Where ST_Distance_Sphere(Point(:userLon, :userLat), Point(s.longitude, s.latitude)) <= :distance " +
+            "ORDER BY s.created_at DESC",
+            countQuery = "Select count(*) From store s " +
+                    "Where ST_Distance_Sphere(Point(:userLon, :userLat), Point(s.longitude, s.latitude)) <= :distance",
+            nativeQuery = true)
+    Page<Store> findStoresWithinRadius(
+            @Param("userLat") BigDecimal userLat,
+            @Param("userLon") BigDecimal userLon,
+            @Param("distance") double distanceInMeters,
+            Pageable pageable); // 기본 조회 + 반경 + 페이징
+
+    Page<Store> findAllByTagOrderByCreatedAtDesc(Tag tag, Pageable pageable); // 태그 별 조회 + 페이징
+
+    @Query(value = "Select * From store s " +
+            "Where ST_Distance_Sphere(Point(:userLon, :userLat), Point(s.longitude, s.latitude)) <= :distance " +
+            "And s.tag = :tag " +
+            "Order By s.created_at DESC",
+            countQuery = "Select count(*) From store s " +
+                    "Where ST_Distance_Sphere(Point(:userLon, :userLat), Point(s.longitude, s.latitude)) <= :distance " +
+                    "And s.tag = :tag",
+            nativeQuery = true)
+    Page<Store> findStoresByTagWithinRadius(
+            @Param("userLat") BigDecimal userLat,
+            @Param("userLon") BigDecimal userLon,
+            @Param("distance") double distanceInMeters,
+            @Param("tag") String tag,
+            Pageable pageable); // 태그 별 조회 + 반경 + 페이징
+
 }
