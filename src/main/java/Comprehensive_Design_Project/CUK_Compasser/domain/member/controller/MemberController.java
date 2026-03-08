4f5001d82e09c1ae3a8c6de74515d700afc9c0c2
jijysun <jijysun@gmail.com>
2026-03-09 08:05:09 +0900

    feat - 적립 API 대비 계층 코드 구현 및 build.gradle 의존성 부여

---
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/member/controller/MemberController.java
@@ -0,0 +1,13 @@
+package Comprehensive_Design_Project.CUK_Compasser.domain.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+
+}
---
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/member/service/MemberService.java
@@ -0,0 +1,14 @@
+package Comprehensive_Design_Project.CUK_Compasser.domain.member.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MemberService {
+
+
+
+}
