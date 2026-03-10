b496a56889accf62f8c47357ff6bc74541ee431e
jijysun <jijysun@gmail.com>
2026-03-10 17:31:14 +0900

    feat - RewardHistory 추가

---
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/reward/entity/RewardHistory.java
@@ -0,0 +1,28 @@
+package Comprehensive_Design_Project.CUK_Compasser.domain.reward.entity;
+
+
+import Comprehensive_Design_Project.CUK_Compasser.domain.member.entity.Member;
+import Comprehensive_Design_Project.CUK_Compasser.domain.store.entity.Store;
+import Comprehensive_Design_Project.CUK_Compasser.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class RewardHistory extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RewardType type; // EARN or USE
+
+}
---
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/reward/entity/RewardType.java
@@ -0,0 +1,5 @@
+package Comprehensive_Design_Project.CUK_Compasser.domain.reward.entity;
+
+public enum RewardType {
+    EARN, COUPON
+}
