898e5ad99586b6bc93f09ee076e7b9016313176d
jijysun <jijysun@gmail.com>
2026-03-10 18:04:11 +0900

    feat - 사징님 일일 적립/쿠폰 횟수 조회 로직 구현

---
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/reward/dto/RewardSummary.java
@@ -0,0 +1,6 @@
+package Comprehensive_Design_Project.CUK_Compasser.domain.reward.dto;
+
+public record RewardSummary (
+    long earnCount,
+    long couponCount
+) {}
--- src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/rewardHistory/repository/RewardHistoryRepository.java
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/rewardHistory/repository/RewardHistoryRepository.java
@@ -1,8 +1,11 @@
 package Comprehensive_Design_Project.CUK_Compasser.domain.rewardHistory.repository;
 
 import Comprehensive_Design_Project.CUK_Compasser.domain.rewardHistory.entity.RewardHistory;
+import Comprehensive_Design_Project.CUK_Compasser.domain.rewardHistory.entity.RewardType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RewardHistoryRepository extends JpaRepository<RewardHistory, Long> {
+import java.time.LocalDateTime;
 
+public interface RewardHistoryRepository extends JpaRepository<RewardHistory, Long> {
+    long countByStore_IdAndTypeAndCreatedAtBetween(Long storeId, RewardType type, LocalDateTime start, LocalDateTime end);
 }
