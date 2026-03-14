34d1c0e77f563e2b7ebcfc572394122aa50492b4
jijysun <jijysun@naver.com>
2026-03-14 22:43:36 +0900

    feat - projection 개선

--- src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/reward/repository/RewardRepository.java
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/reward/repository/RewardRepository.java
@@ -1,8 +1,10 @@
 package Comprehensive_Design_Project.CUK_Compasser.domain.reward.repository;
 
 import Comprehensive_Design_Project.CUK_Compasser.domain.reward.entity.Reward;
+import Comprehensive_Design_Project.CUK_Compasser.domain.storeManager.dto.resp.RewardRespRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +16,13 @@ public interface RewardRepository extends JpaRepository<Reward, Long> {
     @Query("select r from Reward r join fetch Store s where r.member.id = :memberId")
     List<Reward> findAllByMember_Id(Long memberId);
 
+    @Query("SELECT new Comprehensive_Design_Project.CUK_Compasser.domain.storeManager.dto.resp.RewardRespRecord(" +
+            "m.id, m.nickname, s.id, r.id, r.stamp, r.coupon, r.createdAt) " +
+            "FROM Member m, Store s " +
+            "LEFT JOIN Reward r ON r.member = m AND r.store = s " +
+            "WHERE m.id = :memberId AND s.storeManager.id = :storeManagerId")
+    Optional<RewardRespRecord> findRewardRecord(
+            @Param("memberId") Long memberId,
+            @Param("storeManagerId") Long storeManagerId
+    );
 }
