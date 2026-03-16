913f124f7959922f8e1adabfb428334285e5b9dc
jijysun <jijysun@gmail.com>
2026-03-16 23:16:50 +0900

    fix - 적립 조회 데이터 변경 대비 record 변경

--- src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/storeManager/dto/resp/RewardRespRecord.java
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/storeManager/dto/resp/RewardRespRecord.java
@@ -5,10 +5,25 @@ import java.time.LocalDateTime;
 public record RewardRespRecord(
         Long memberId,
         String nickname,
+        String email,
         Long storeId,
+        String boxName,
+        Integer totalPrice,
         Long rewardId,
         Integer stamp,
         Integer coupon,
         LocalDateTime createdAt
 ) {
 }
+/*
+ * 반환해야 하는 정보
+ * member: nickname, email // memberId
+ * store: store_id // store_manager.getStore().getId()
+ * order: totalPrice // findByMemberIdAndStoreId, randomBox와 연관 설정 예정.
+ * randomBox: boxName // order.getRandomBox().getName()
+ * reward: stamp, coupon // findByMemberIdAndStoreId
+ * ----
+ * 주어지는 정보: memberId, storeManagerId
+ * findRewardRespRecordByMemberIdAndStoreManagerId
+ *
+ * */
