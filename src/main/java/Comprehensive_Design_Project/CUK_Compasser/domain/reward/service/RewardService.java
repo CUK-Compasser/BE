904c4cfd4e288d61980ace9930f3fc6a83303f87
jijysun <jijysun@gmail.com>
2026-03-17 13:14:25 +0900

    docs - 적립 서비스 관련 javadoc 추가

--- src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/reward/service/RewardService.java
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/domain/reward/service/RewardService.java
@@ -23,6 +23,13 @@ public class RewardService {
     /*
     * 사장님 홈 페이지에서 사용할 DTO!
     * */
+
+    /**
+     * 사장님 홈페이지에서 사용항 DTO 반환 Method
+     * @author jijysun
+     * @param storeId
+     * @return domain.reward.dto.RewardSummary
+     */
     @Transactional(readOnly = true)
     public RewardSummary getStoreRewardInToday (Long storeId){
 
@@ -35,6 +42,14 @@ public class RewardService {
         return new RewardSummary(earnCount, couponCount);
     }
 
+
+    /**
+     * 사용자 마이페이제서 사용할 DTO 반환 Method
+     * Order 변경 후 사용해주시면 됩니다.
+     * @author jijysun
+     * @param memberId
+     * @return domain.reward.dto.SummaryMemberReward
+     */
     @Transactional(readOnly = true)
     public SummaryMemberReward summaryMemberReward(Long memberId) {
         // reward -> select sum (r.stamp), sum (r.useCouponCnt) from Reward r where r.member.id = :memberId
