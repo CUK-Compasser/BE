1760b2bde75494bc44e5e951439c8e6ffe0f7622
shawn9272 <shawn9272@naver.com>
2026-03-19 15:22:31 +0900

    chore - spring boot 3.x Statistics 엔드포인트 예외 처리

--- src/main/java/Comprehensive_Design_Project/CUK_Compasser/global/security/config/SecurityConfig.java
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/global/security/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
                                 "/auth/login/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/actuator/**"
                         ).permitAll()
                         .requestMatchers("/stores/**").authenticated()
                         .requestMatchers("/owners/**").authenticated()
