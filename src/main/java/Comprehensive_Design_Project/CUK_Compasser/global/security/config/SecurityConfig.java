f85aa2e26e34531c8f0fe15c79e55a2eb63d95c5
shawn9272 <shawn9272@naver.com>
2026-03-20 18:18:34 +0900

    모니터링 경로 검사 제외 설정 추가

--- src/main/java/Comprehensive_Design_Project/CUK_Compasser/global/security/config/SecurityConfig.java
+++ src/main/java/Comprehensive_Design_Project/CUK_Compasser/global/security/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -60,5 +61,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // prometheus 모니터링 경로는 검사에서 제외
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(
+                        "/actuator/**",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/favicon.ico"
+                );
     }
 }
